607616b5bd9b5d55974ff7b4763805cb9e85db5a
ecubs91 <harrylee1230@gmail.com>
2014-07-30 18:12:06 +0100

    correct syntax in welcome_mailer

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -2,7 +2,7 @@ class WelcomeMailer < ActionMailer::Base
   default from: "harrylee1230@gmail.com"
 
   def registration_confirmation(user)
-  	mail :to ==> user, :subject => "Thank you for signing up with Young Tutors"
+  	mail :to ==> user, :subject ==> "Thank you for signing up with Young Tutors"
   end
 
 end
