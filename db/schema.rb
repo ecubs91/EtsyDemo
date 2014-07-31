4696fa103476aad94415b5aa97394de7b642921b
ecubs91 <harrylee1230@gmail.com>
2014-07-31 17:36:16 +0100

    integrate name and date to reviews on listing show page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140730163643) do
+ActiveRecord::Schema.define(version: 20140731160708) do
 
   create_table "listings", force: true do |t|
     t.string   "name"
@@ -70,6 +70,8 @@ ActiveRecord::Schema.define(version: 20140730163643) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
